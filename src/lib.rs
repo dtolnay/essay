a69cc91faf69b46e76e9c5190736f2edf43f2f70
David Tolnay <dtolnay@gmail.com>
2019-09-30 21:47:45 -0400

    Switch to avatar with transparency

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(non_snake_case)]
-#![doc(html_logo_url = "https://avatars1.githubusercontent.com/u/1940490?s=460&v=4")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/dtolnay/essay/avatar/avatar.png")]
 
 #[path = "2019-08-08-await-a-minute.rs"]
 mod _01;
